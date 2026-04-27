1c16419118a023091824eefda05d862c17ae2086
sarthak180321 <168582310+sarthak180321@users.noreply.github.com>
2026-04-27 10:38:00 +0530

    Add files via upload

---
+++ EndSemPracticalExam/Floyd.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+int main(){
+    int n;
+    cin>>n;
+    vector<vector<int>> graph(n,vector<int>(n));
+    for(int i=0;i<n;i++){
+        for(int j=0;j<n;j++){
+            cin>>graph[i][j];
+        }
+    }
+    for(int k=0;k<n;k++){
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n;j++){
+                if(graph[i][j]>graph[i][k]+graph[k][j]){
+                    graph[i][j]=graph[i][k]+graph[k][j];
+                }
+            }
+        }
+    }
+    for(int i=0;i<n;i++){
+        for(int j=0;j<n;j++){
+            cout<<graph[i][j]<<" ";
+        }
+        cout<<endl;
+    }
+}
